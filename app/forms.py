e3fa537d12fedecd760af28c9a5c3d8ac3554f2e
unknown <ethioZele4443@gmail.com>
2023-11-02 23:46:36 -0800

    first commit

---
+++ app/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from app.models import FaceRecognition
+
+class FaceRecognitionform(forms.ModelForm):
+    class Meta:
+        model = FaceRecognition
+        fields =['image']
